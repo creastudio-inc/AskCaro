c945eb50d84568a8780dbc4e60c22003cc2a087a
ahmed Oumezzine <ahmedoumezzine@outlook.fr>
2019-06-29 22:29:51 +0100

    desgin admin theme

---
+++ AskCaro/Areas/Administrator/Controllers/ServerFaultController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AskCaro.Areas.Administrator.Controllers
+{
+    [Area("Administrator")]
+
+    public class ServerFaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Tag()
+        {
+            return View();
+        }
+
+        public IActionResult Statistics()
+        {
+            return View();
+        }
+
+
+        public IActionResult Questions()
+        {
+            return View();
+        }
+
+        public IActionResult Tags()
+        {
+            return View();
+        }
+
+        public IActionResult AddTag()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Scheduler()
+        {
+            return View();
+        }
+    }
+}
---
+++ AskCaro/Models/MenuHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace AskCaro
+{
+    /// <summary>
+    /// Menu current active item helper class.
+    /// </summary>
+    public static class MenuHelper
+    {
+        /// <summary>
+        /// Determines whether the specified controller is selected.
+        /// </summary>
+        /// <param name="html">The HTML.</param>
+        /// <param name="controller">The controller.</param>
+        /// <param name="action">The action.</param>
+        /// <param name="cssClass">The CSS class.</param>
+        /// <returns></returns>
+        public static string IsSelected(this Microsoft.AspNetCore.Mvc.Rendering.IHtmlHelper html, string controller = null, string action = null, string cssClass = "selected")
+        {
+            // const string cssClass = "selected";
+            var currentAction = (string)html.ViewContext.RouteData.Values["action"];
+            var currentController = (string)html.ViewContext.RouteData.Values["controller"];
+
+            if (String.IsNullOrEmpty(controller))
+                controller = currentController;
+
+            if (String.IsNullOrEmpty(action))
+                action = currentAction;
+
+            return controller == currentController && action == currentAction ?
+                cssClass : String.Empty;
+        }
+
+    }
+}
